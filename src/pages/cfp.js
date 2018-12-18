0d332f06429a875350ff56762d04aa1c6e07d70a
dieguich <dcasado@deusto.es>
2018-12-18 14:06:55 +0100

    simple change CFP

--- src/pages/cfp.js
+++ src/pages/cfp.js
@@ -4,17 +4,19 @@ import cautiousDog from "../images/cautious-dog.svg";
 
 const CFPPage = () => (
   <div className="flex flex-col md:flex-row items-center">
-    <div className="md:w-2/3 md:mr-8">
+    <div className="md:w-3/3 md:mr-8">
       <p className="border-l-4 border-grey-darkest font-serif leading-loose pl-4 text-justify">
-        CFP
+        Driven by connected low-cost sensing and actuation and the large host of scientifically and economically relevant application scenarios that they enable, 
+        the Internet of Things has become a central research area with a broad reach across many fields in information technology and adjacent domains.
+        The <strong>International Conference on Internet of Things </strong> (IoT) has become the premier gathering place where visionary, ground-breaking research in the IoT field meets leading industry experts. 
+        Since its beginnings in the year 2008, IoT has been backed by strong support from leading academic institutions as well as industry and we are happy to announce its 9th
+        iteration will take place from October 22-25th 2018 in Bilbao, Spain.
       </p>
-      <p className="font-bold mt-4 text-right text-xs uppercase">
-        — Thomas Nagel
-      </p>
-    </div>
 
-    <div className="w-2/3 md:w-1/3">
-      <img src={cautiousDog} />
+      <p className="border-l-4 border-grey-darkest font-serif leading-loose pl-4 text-justify">
+      
+      </p>      
+      
     </div>
   </div>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -31,17 +31,31 @@ const IndexPage = () => (
     <div className="flex">
     <div className="md:w-2/3 md:mr-8">
       <p className="font-serif leading-loose pl-4 text-justify">
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      
+      The 9th International Conference on the Internet of Things (IoT 2019), building on the success of its predecessors since 2008, is the premier forum for such efforts, to share, 
+      discuss and witness cutting edge research in all areas of development for the Internet of Things.
+
+      </p><br/>
+      <p className="font-serif leading-loose pl-4 text-justify">
+      Pervasive connectivity, smart devices and demand for data testify to an IoT that will continue to grow by leaps and bounds. Computing power is dropping in price while new sensors 
+      are being developed and incorporated into everyday objects, and as people buy into IoT technology, economies of scale lend themselves to the creation of ever more 
+      data-centric businesses and applications. Instrumenting and connecting devices has massive potential to deliver a social and economic value.  
+      However, there is need for a coordinated effort when rolling out the next generation of self-reporting paradigms. 
+      </p>
+      <br/>
+
+      <p className="font-serif leading-loose pl-4 text-justify">
+      The Internet of Things Conference is seeking original, high impact research papers on all topics related to the development of the Internet of Things. Papers will be reviewed 
+      and selected based on technical novelty, integrity of the analysis and social-environmental impacts and practical relevance.
       </p>
     </div>
     <div className="md:w-2/3 md:mr-8">
       <p className="border-l-4 border-green font-serif leading-loose pl-4 text-justify">
       <h3>Paper Submission Key Dates:</h3>
       <ul className="list-restet">
-        <li><strong>Abstract and title due:</strong> Tuesday, January 15, 2019</li>
-        <li><strong>Paper submission deadline:</strong> Friday, January 18, 2019</li>
-        <li><strong>Papers camera ready deadline:</strong> Wednesday, April 17, 2019</li>
-        <li><strong>Conference:</strong> June 23-28, 2019</li>
+        <li><strong>Paper submission deadline:</strong> Friday, May 3, 2019</li>
+        <li><strong>Papers camera ready deadline:</strong> Thursday, July 4, 2019</li>
+        <li><strong>Conference:</strong> October 22-25, 2019</li>
       </ul>
       </p>
     </div>
@@ -52,7 +66,7 @@ const IndexPage = () => (
     <div className="flex bg-grey-lighter">
       <div className="flex-1 text-grey-darker text-center bg-grey-light px-30 py-2 m-2"><img src={ACM} /></div>
       <div className="flex-1 text-grey-darker text-center bg-grey-light px-30 py-2 m-2"><img src={CHI} /></div>
-      <div className="flex-1 text-grey-darker text-center bg-grey-light px-30 py-2 m-2"><img src={ICPS} /></div>
+      {/*<div className="flex-1 text-grey-darker text-center bg-grey-light px-30 py-2 m-2"><img src={ICPS} /></div>*/}
     </div>
 
     <h3>Sponsors</h3>
