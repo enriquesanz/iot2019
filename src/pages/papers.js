16e7106a82518c4815d568470b6a36169bb0cda9
Enrique Sanz <enriquesanz@gmail.com>
2018-12-20 12:57:27 +0100

    CFP a medias

---
+++ src/pages/papers.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Link from "gatsby-link";
+import cautiousDog from "../images/cautious-dog.svg";
+
+const CFPPage = () => (
+  <div className="flex flex-col md:flex-row items-center">
+    <div className="md:w-3/3 md:mr-8">
+      <p className="border-l-4 border-grey-darkest font-serif leading-loose pl-4 text-justify">
+      <h1>Full and short papers</h1>
+      The International Conference on Internet of Things (IoT) has become the premier gathering place where visionary, ground-breaking research in the IoT field meets leading industry experts. Since its beginnings in the year 2008, IoT has been backed by strong support from leading academic institutions as well as industry and we are happy to announce its 9th iteration will take place from October 22-25th 2018 in Bilbao, Spain.
+<br />
+<br />
+The topic of this year edition will be Internet of Things for People which encompasses all the applications and ground-breaking work that is being done in the IoT field to pave the bridge between citizens and everywhere data produced by pervasive technology. 
+
+In general terms, IoT 2019 is seeking for original, high impact research papers on all topics related to the development and social adoption of the Internet of Things. Papers will be reviewed and selected based on technical novelty, integrity of the analysis and social impacts and practical relevance.  Recommended topics of submission are as follows, but not limited to.
+
+      <br />
+      <br />
+      The topic of this year edition will be Internet of Things for People which encompasses all the applications and ground-breaking work that is being done in the IoT field to pave the bridge between citizens and everywhere data produced by pervasive technology. 
+
+      </p>
+
+    <br />
+
+    <div className="flex">
+      <p className="font-serif leading-loose pl-4 text-justify">
+      <h2>TOPICS</h2>
+      <ul>
+      <li>Internet of Things Architecture: Novel information architecture design on field, edge, and cloud devices; object access and networking technology; investigation of technologies that support the mobility of functions and executions across system entities.</li>
+      <li>Interoperability of IoT Systems: Service discovery and composition, synchronization in distributed systems, overcoming siloization of IoT systems, semantic data description frameworks.</li>
+      <li>Description and Discovery of IoT devices, including mobile and moving devices: Ontologies and data models for the description and discovery of mobile systems and applications, including autonomous systems and wearable, urban or sensory computing devices.</li>
+    </ul>
+    </p>
+    </div>
+
+    </div>
+ </div>
+
+  
+);
+
+export default CFPPage;
--- src/pages/wips.js
+++ src/pages/wips.js
@@ -6,7 +6,7 @@ const CFPPage = () => (
   <div className="flex flex-col md:flex-row items-center">
     <div className="md:w-3/3 md:mr-8">
       <p className="border-l-4 border-grey-darkest font-serif leading-loose pl-4 text-justify">
-      <h1>Call for papers</h1>
+      <h1>WIPS</h1>
       Driven by connected low-cost sensing and actuation and the large host of scientifically and economically relevant application scenarios that they enable, the Internet of Things has become a central research area with a broad reach across many fields in information technology and adjacent domains. The International Conference on Internet of Things (IoT) has become the premier gathering place where visionary, ground-breaking research in the IoT field meets leading industry experts. Since its beginnings in the year 2008, IoT has been backed by strong support from leading academic institutions as well as industry and we are happy to announce its 9th iteration will take place from October 22-25th 2018 in Bilbao, Spain.
       <br />
       <br />
@@ -26,7 +26,7 @@ const CFPPage = () => (
         </ul>
 
 
-          <a href="/wips"><h2>WiPs and Demos</h2></a>
+          <h2>WiPs and Demos</h2>
           <ul>
             <li>Due: June 28, 2019</li>
             <li>Notifications: July 30, 2019</li>
@@ -37,14 +37,14 @@ const CFPPage = () => (
 
       <p className="font-serif leading-loose pl-4">
 
-          <a href="/doctoral"><h2>Doctoral consortium</h2></a>
+          <h2>Doctoral consortium</h2>
           <ul>
             <li>Due: Friday, June 28, 2019</li>
             <li>Notifications: July 16, 2019</li>
             <li>Camera Ready: July 30, 2019</li>
             </ul>
 
-          <a href="/workshops"><h2>Workshops proposals</h2></a>
+          <h2>Workshops proposals</h2>
           <ul>
             <li>Proposals Due: March 25, 2019</li>
             <li>Notifications: April 12, 2019</li>
