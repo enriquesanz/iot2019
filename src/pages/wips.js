17fdc875f866e1266ca4975fc765a57916ebff57
Enrique Sanz <enriquesanz@gmail.com>
2018-12-20 15:05:24 +0100

    Wips done

--- src/pages/wips.js
+++ src/pages/wips.js
@@ -6,56 +6,60 @@ const CFPPage = () => (
   <div className="flex flex-col md:flex-row items-center">
     <div className="md:w-3/3 md:mr-8">
       <p className="border-l-4 border-grey-darkest font-serif leading-loose pl-4 text-justify">
-      <h1>WIPS</h1>
-      Driven by connected low-cost sensing and actuation and the large host of scientifically and economically relevant application scenarios that they enable, the Internet of Things has become a central research area with a broad reach across many fields in information technology and adjacent domains. The International Conference on Internet of Things (IoT) has become the premier gathering place where visionary, ground-breaking research in the IoT field meets leading industry experts. Since its beginnings in the year 2008, IoT has been backed by strong support from leading academic institutions as well as industry and we are happy to announce its 9th iteration will take place from October 22-25th 2018 in Bilbao, Spain.
-      <br />
-      <br />
-      The topic of this year edition will be Internet of Things for People which encompasses all the applications and ground-breaking work that is being done in the IoT field to pave the bridge between citizens and everywhere data produced by pervasive technology. 
+      <h1>Work-in-Progress and Demo sessions</h1>
+      The objective of the WiPs and demo sessions is to give researchers in the emerging research areas of IoT and their applications the opportunity to present early and fresh, in-progress results in order to share experiences and strategies whilst facilitating the discussion between authors and conference attendees.
 
+<br />
+<br />
+The topic of this year edition will be Internet of Things for People which encompasses all the applications and ground-breaking work that is being done in the IoT field to pave the bridge between citizens and everywhere data produced by pervasive technology.
+
+In general terms, IoT 2019 is seeking for works on all topics related to the development and social adoption of the Internet of Things.  Recommended topics of submission are as follows, but not limited to.
+
+      <br />
       </p>
 
     <br />
-      <div className="flex">
-      <p className="font-serif leading-loose pl-4">
-      
-      <a href="/papers"><h2>Full and short papers</h2></a>
+
+    <div className="flex">
+      <p className="font-serif leading-loose pl-4 text-justify">
+      <h2>TOPICS</h2>
+      <ul>
+        <li>Internet of Things Architecture: Novel information architecture design on field, edge, and cloud devices; object access and networking technology; investigation of technologies that support the mobility of functions and executions across system entities.</li>
+        <li>Interoperability of IoT Systems: Service discovery and composition, synchronization in distributed systems, overcoming siloization of IoT systems, semantic data description frameworks.</li>
+        <li>Description and Discovery of IoT devices, including mobile and moving devices: Ontologies and data models for the description and discovery of mobile systems and applications, including autonomous systems and wearable, urban or sensory computing devices.</li>
+      </ul>
+      <p className="text-xl text-center">~~~</p>
+      <ul>
+        <li>Novel IoT Interactions: Novel methods and techniques for seamless human-to-object and object-to-object interactions, including, but not limited to brain, audible, tangible or augmented or mixed reality.</li>
+        <li>Object-objects Interactions: methods and protocols for co-operations and coordination among objects, social internet of things, trust among objects</li>
+        <li>Social Adoption of IoT Systems: Data security, authentication, authorization and trustworthiness. Privacy protection, data sharing technologies (incl. blockchain technology), tampering protection and detection in IoT systems.</li>
+        <li>Internet of People: the new IoT paradigm where humans and their personal devices are not seen merely as end users of applications, but become active elements of the IoT. Thus, collaboration bridging machine intelligence and human intelligence</li>
+      </ul>
+      <p className="text-xl text-center">~~~</p>
+      <ul>
+        <li>Web Technologies for the IoT: Web-based discovery, search, and service composition to facilitate interactions between devices on the IoT and with users.</li>
+        <li>Physical World Event Processing and Understanding: Novel data collection, deep learning, reality mining, and prediction methods based on physical world observations. This might include real-time decision making, event processing, and extracting information from large datasets.</li>
+        <li>Integration of Physical and Virtual Artifacts and Events: Advanced object identification, classification, and localization.</li>
+        <li>Real World Applications of IoT technology: Evaluation of challenges of real world deployments of Industry 4.0, Industrial Internet, and GS1 implementations, including planned deployments (e.g., in advanced Manufacturing and Logistics).</li>
+      </ul>
+      <h2>IMPORTANT DATES</h2>
         <ul>
-          <li>Submissions: Friday, May 3, 2019</li>
-          <li>Notifications: Friday, June 21, 2019 </li>
-          <li>Camera ready: Thursday, July 4, 2019</li>
-        </ul>
-
-
-          <h2>WiPs and Demos</h2>
-          <ul>
             <li>Due: June 28, 2019</li>
             <li>Notifications: July 30, 2019</li>
             <li>Camera Ready: September 2, 2019 </li>
-          </ul>
+          <li>Conference: October 22-25, 2019</li>
+        </ul>
 
-      </p>
+        <h2>Preparing and Submitting Your Papers:</h2>
+        Papers have to be submitted via the EasyChair conference system, must be written in English and contain original material that has not been published or is currently undergoing review elsewhere. Papers should not exceed 8 pages, including figures and references. The paper layout should follow the SIGCHI Conference format. Papers are peer-reviewed by a committee of experts in the IoT field and selected based on technical novelty, integrity of the analysis, and practical relevance and impact. Accepted papers will be listed on DBLP and published via the ACM Digital Library. Cases of plagiarism or multiple submissions will be subject to disciplinary action as per ACM rules and regulations, and no-shows at the conference will result in an exclusion from the ACM Digital Library. Selected papers will be invited to extend and improve their contributions to  Special Issues under consideration.
 
-      <p className="font-serif leading-loose pl-4">
-
-          <h2>Doctoral consortium</h2>
-          <ul>
-            <li>Due: Friday, June 28, 2019</li>
-            <li>Notifications: July 16, 2019</li>
-            <li>Camera Ready: July 30, 2019</li>
-            </ul>
-
-          <h2>Workshops proposals</h2>
-          <ul>
-            <li>Proposals Due: March 25, 2019</li>
-            <li>Notifications: April 12, 2019</li>
-            <li>Camera Ready(online-information in web): April 30, 2019</li>
-          </ul>
-
-
-      </p>      
-      </div>
+    </p>
     </div>
-  </div>
+
+    </div>
+ </div>
+
+  
 );
 
 export default CFPPage;
