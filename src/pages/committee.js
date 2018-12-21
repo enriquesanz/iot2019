3993abfc1a2899bda8d7d52b3c0953028ca6c12e
Enrique Sanz <enriquesanz@gmail.com>
2018-12-21 11:52:36 +0100

    Committee terminado

--- src/pages/committee.js
+++ src/pages/committee.js
@@ -1,22 +1,95 @@
 import React from "react";
 import Link from "gatsby-link";
-import cautiousDog from "../images/cautious-dog.svg";
 
-const AboutPage = () => (
-  <div className="flex flex-col md:flex-row items-center">
+const CommitteePage = () => (
+    <div className="flex flex-col md:flex-row items-center">
     <div className="md:w-2/3 md:mr-8">
       <p className="border-l-4 border-grey-darkest font-serif leading-loose pl-4 text-justify">
-        Committee
+        <h1>Committee</h1>
       </p>
-      <p className="font-bold mt-4 text-right text-xs uppercase">
-        — Thomas Nagel
-      </p>
-    </div>
+      <p className="font-serif leading-loose pl-4 text-justify">
+        <h2>Conference Chairs:</h2>
+        <ul>
+          <li>Diego López de Ipiña (University of Deusto, Spain)</li>
+          <li>Kyriakos G. Vamvoudakis (Georgia Institute of Technology, USA)</li>
+        </ul>
 
-    <div className="w-2/3 md:w-1/3">
-      <img src={cautiousDog} />
+        <h2>Technical Program Chairs:</h2>
+        <ul>
+          <li>Karin Anna Hummel (JKU Linz, Austria)</li>
+          <li>David Boyle (Imperial College, UK)</li>
+          <li>Matthias Kovatsch (Huawei, Germany)</li>
+        </ul>
+
+        <h2>Demo/Poster Chair:</h2>
+        <ul>
+          <li>Kimberly Garcia (Siemens, USA)</li>
+          <li>Frank A. Kraemer (NTNU, Norway)</li>
+        </ul>
+
+        <h2>Workshop Chairs:</h2>
+        <ul>
+          <li>Antonio Jara (HES-SO, Switzerland)</li>
+          <li>J. Martin Serrano (Insight NUI Galway, Ireland)</li>
+        </ul>
+
+        <h2>Doctoral Colloquium Chairs:</h2>
+        <ul>
+          <li>Iñaki Vazquez (University of Deusto, Spain)</li>
+          <li>Simon Mayer (University of St. Gallen, Switzerland)</li>
+        </ul>
+
+        <h2>Local Liaison Chairs:</h2>
+        <ul>
+          <li>Diego Casado-Mansilla (University of Deusto, Spain)</li>
+        </ul>
+
+        <h2>Industrial Chairs:</h2>
+        <ul>
+          <li>Josu Bilbao (Ikerlan, Spain)</li>
+          <li>TBA</li>
+        </ul>
+
+        <h2>Spanish Assembly Chairs:</h2>
+        <ul>
+          <li>Aitor Urbieta (Ikerlan, Spain)</li>
+          <li>Felix Villanueva (UCLM, Spain)</li>
+        </ul>
+
+        <h2>Web Chairs:</h2>
+        <ul>
+          <li>Anne M. Irizar (University of Deusto, Spain)</li>
+          <li>Enrique Sanz (University of Deusto, Spain)</li>
+        </ul>
+
+        <h2>Publicity/Social Media Chairs:</h2>
+        <ul>
+          <li>TBA</li>
+        </ul>
+
+        <h2>Steering committee:</h2>
+        <ul>
+          <li>Elgar Fleisch (ETH Zurich / St. Gallen, Switzerland)</li>
+          <li>Alexander Ilic (Univ of St Gallen, Switzerland)</li>
+          <li>Christian Floerkemeier (MIT / ETH Zurich, U.S.A/Switzerland)</li>
+          <li>Marc Langheinrich (Università della Svizzera italiana – USI, Switzerland)</li>
+          <li>Friedemann Mattern (ETH Zurich, Switzerland)</li>
+          <li>Florian Michahelles (Siemens, USA)</li>
+          <li>Jun Murai (Keio University, Japan)</li>
+          <li>Sanjay Sarma (MIT, U.S.A)</li>
+        </ul>
+
+        <h2>Program Committee:</h2>
+        <ul>
+          <li>TBA</li>
+        </ul>
+
+      </p>
     </div>
   </div>
+
+
+  
 );
 
-export default AboutPage;
+export default CommitteePage;
